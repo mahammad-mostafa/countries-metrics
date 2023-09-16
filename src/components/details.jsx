b518d6bff928e5992b957f877424a6e6f06fe6d3
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-09-16 22:52:02 +0300

    Structured item details with classes for styling

--- src/components/details.jsx
+++ src/components/details.jsx
@@ -1,28 +1,47 @@
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate, useParams } from 'react-router-dom';
+import Indicator from './indicator';
+import Styles from '../styles/details.module.css';
 
 const Details = () => {
   const { name } = useParams();
+  const [flipped, setFlipped] = useState(false);
   const error = useSelector((state) => state.countries.error);
   const countries = useSelector((state) => state.countries.list);
   const loading = useSelector((state) => state.countries.loading);
+  useEffect(() => setFlipped(true), []);
   if (loading) {
-    return <h2>Loading</h2>;
+    return <Indicator loading="true" />;
   }
   if (error !== null) {
-    return <h2>{error}</h2>;
+    return <Indicator message={error} />;
   }
   const country = countries.find((item) => item.name.replace(/ /g, '-').toLowerCase() === name);
   if (country === undefined) {
     return <Navigate to="/404" />;
   }
   return (
-    <section>
-      <img src={country.flag} alt={country.name} />
-      <h2>{country.name}</h2>
-      <span>{country.capital}</span>
-      <p>{country.region}</p>
-      <p>{country.population}</p>
+    <section className={Styles.section}>
+      <img className={Styles.flag} src={country.flag} alt={country.name} />
+      <div className={`${Styles.labels} ${flipped ? null : Styles.flip}`}>
+        <span>Name:</span>
+        <span className={Styles.text}>{country.name}</span>
+      </div>
+      <div className={`${Styles.labels} ${flipped ? null : Styles.flip}`}>
+        <span>Capital:</span>
+        <span className={Styles.text}>{country.capital}</span>
+      </div>
+      <div className={`${Styles.labels} ${flipped ? null : Styles.flip}`}>
+        <span>Population:</span>
+        <span className={Styles.text}>{country.population}</span>
+      </div>
+      <div className={`${Styles.labels} ${flipped ? null : Styles.flip}`}>
+        <span>Timezone:</span>
+        <span className={Styles.text}>
+          {country.timezones.map((timezone) => <div key={timezone}>{timezone}</div>)}
+        </span>
+      </div>
     </section>
   );
 };
