aa0d63f914da05148d9446d2616b9ddae45de7f7
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-09-16 22:50:58 +0300

    Added text content & jsx structure

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,3 +1,12 @@
-const About = () => <h1>About</h1>;
+import Styles from '../styles/about.module.css';
+
+const About = () => (
+  <section className={Styles.text}>
+    <h2 className={Styles.title}>About</h2>
+    <p>This is the capstone project of the third module in the Microverse program.</p>
+    <p>It is a metrics app for countries built with React framework.</p>
+    <p>State mangament & network calls are handled by Redux toolkit.</p>
+  </section>
+);
 
 export default About;
